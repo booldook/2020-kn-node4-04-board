157edad60fc5809a84b6f14ed21fe5a8139d4546
booldook <booldook@gmail.com>
2020-10-30 21:22:25 +0900

    pug 생성

--- routes/board.js
+++ routes/board.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-
+router.get(['/', '/list'], (req, res, next) => {
+	const pug = {title: '게시판 리스트'}
+	res.render('./board/list.pug', pug);
+});
 
 module.exports = router;
