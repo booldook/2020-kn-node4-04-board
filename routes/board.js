652e82438509d0a4722ac5a2e2d35a4585a3ff58
booldook <booldook@gmail.com>
2020-10-30 22:02:00 +0900

    board

--- routes/board.js
+++ routes/board.js
@@ -6,4 +6,9 @@ router.get(['/', '/list'], (req, res, next) => {
 	res.render('./board/list.pug', pug);
 });
 
+router.get('/write', (req, res, next) => {
+	const pug = {title: '게시글 작성', js: 'board', css: 'board'};
+	res.render('./board/write.pug', pug);
+});
+
 module.exports = router;
