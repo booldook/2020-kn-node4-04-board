4564c8b2495381ee2e687ac46b5623f0cefb4edf
booldook <booldook@gmail.com>
2020-10-30 22:16:46 +0900

    board 만들기 시작

--- modules/mysql-conn.js
+++ modules/mysql-conn.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_DATABASE,
 	port: process.env.DB_PORT,
-	waitForConnections:true,
+	waitForConnections: true,
 	connectionLimit: 10
 });
 
--- routes/board.js
+++ routes/board.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { pool } = require('../modules/mysql-conn');
 
 router.get(['/', '/list'], (req, res, next) => {
 	const pug = {title: '게시판 리스트', js: 'board', css: 'board'};
@@ -10,5 +11,22 @@ router.get('/write', (req, res, next) => {
 	const pug = {title: '게시글 작성', js: 'board', css: 'board'};
 	res.render('./board/write.pug', pug);
 });
+
+router.post('/save', async (req, res, next) => {
+	const { title, content, writer } = req.body;
+	var values = [title, content, writer];
+	var sql = 'INSERT INTO board SET title=?, writer=?, content=?';
+	try {
+		const connect = await pool.getConnection();
+		const rs = await connect.query(sql, values);
+		connect.release();
+		res.json(rs);
+	}
+	catch(err) {
+		next(err);
+	}
+});
+
+
 
 module.exports = router;
