022c388aa295ee2ee71d81fd202f027438cadf37
booldook <booldook@gmail.com>
2020-11-10 20:25:39 +0900

    promise

--- modules/multer-conn.js
+++ modules/multer-conn.js
@@ -2,19 +2,27 @@ const multer = require('multer');
 const moment = require('moment');
 const path = require('path');
 const fs = require('fs');
+const fsp = require('fs/promises');
 const { v4: uuidv4 } = require('uuid');
 
-const makeFolder = () => {
+const makeFolder = async () => {
 	let result = { err: null };
 	let folder = path.join(__dirname, '../uploads', moment().format('YYMMDD'));
 	result.folder = folder;
 	if(!fs.existsSync(folder)) {
-		fs.mkdir(folder, (err) => {
-			if(err) result.err = err;
-			return result;
-		});
+		const err = await fsp.mkdir(folder);
+		if(err) result.err = err;
+		return result; 
 	}
 	else return result;
+
+	/* if(!fs.existsSync(folder)) {
+		fs.mkdir(folder, (err) => {
+			if(err) result.err = err;
+			return result; // result = {err: null, folder: '경로'}
+		});
+	}
+	else return result; */
 }
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
