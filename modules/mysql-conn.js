7ac2c7b56ddf57dc262b2b9779bd14f1c5f1b8b6
hi.java2 <hi.java2@gmail.com>
2020-11-13 20:22:11 +0900

    sqlGen

--- modules/mysql-conn.js
+++ modules/mysql-conn.js
@@ -9,4 +9,17 @@ const pool = mysql.createPool({
 	connectionLimit: 10
 });
 
+// mode = 'I', 'U', 'S', 'D'
+// table = 'tableName'
+// field = ['title', 'writer', 'content']
+// data = {title: 'A', content: 'B'} // req.body
+// file = {filename: '201113-.jpg', originalname: 'abc.jpg', size: 1234} // req.file
+// key = id값
+const sqlGen = (obj) => {
+	let { mode, table, field, data, file, key } = obj;
+	const sql = 'INSERT INTO table SET title=?, writer=?'
+	// const sql = 'UPDATE table SET '
+	return { sql, values }
+}
+
 module.exports = { mysql, pool };
