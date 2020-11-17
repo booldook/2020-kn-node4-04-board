2ead42ad6d25f12ff302ec39610e7b0f50c4637d
hi.java2 <hi.java2@gmail.com>
2020-11-17 20:22:33 +0900

    pager 구현중

---
+++ modules/pager-conn.js
@@ -0,0 +1,17 @@
+// let pagers = pager(2, 90, {listCnt: 5, pagerCnt: 5})
+// let pagers = pager(2, 90)
+
+const pager = (page, totalRecord, obj) => {
+	page = Number(page);
+	totalPage = Number(totalPage);
+	let { listCnt=5, pagerCnt=3 } = obj || {};
+	let totalPage = Math.ceil(totalRecord / listCnt);
+	let startIdx = (page - 1) * listCnt;
+	let startPage = Math.floor((page - 1) / pagerCnt) * pagerCnt + 1;
+	let endPage = startPage + pagerCnt - 1 > totalPage ? totalPage : startPage + pagerCnt - 1;
+	let nextPage = page + 1 > totalPage ? totalPage : page + 1;
+	let prevPage = page - 1;
+	return { page, totalRecord, listCnt, pagerCnt, totalPage, startIdx, startPage, endPage, nextPage, prevPage };
+}
+
+module.exports = pager;
